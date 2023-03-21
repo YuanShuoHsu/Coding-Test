f5a6806d73874fd4e9ccd617258e7a682d8c77bd
YuanShuoHsu <79447837+YuanShuoHsu@users.noreply.github.com>
2023-03-21 18:07:37 +0800

    login scss modified

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -4,8 +4,16 @@ import "./index.scss"
 export default function Login() {
     return (
         <div className='Login'>
-            <div className='Login-container'>
-
+            <div className='login__container'>
+                <div className='login__item'>
+                    <h2 className='item__label'>帳號：</h2>
+                    <input className='item__input' type="text" />
+                </div>
+                <div className='login__item'>
+                    <h2 className='item__label'>密碼：</h2>
+                    <input className='item__input' type="text" />
+                </div>
+                <button className='login__button'>登入</button>
             </div>
         </div>
     )
